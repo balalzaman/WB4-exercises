7568c7be352882eda42aee7c5bd333a9560d39df
balalzaman <balalzaman14@gmail.com>
2023-05-12 11:33:17 -0400

    WB4 - exercises

--- ArrayScripts/student_average.js
+++ ArrayScripts/student_average.js
@@ -6,3 +6,25 @@ let students = [
     {name: "Elisha", scores: [89, 100]},
     {name: "Ezra", scores: [100, 99, 100, 87]}
    ];
+
+function studentAverage(allStudents) {
+
+    let numOfStudents = allStudents.length;
+
+    average = 0;
+
+        for (i = 0; i < numOfStudents; i++) {
+            let students = allStudents[i];
+            let numOfScores = students.scores.length;
+            let total = 0;
+                
+                for (s = 0; s < numOfScores; s++){
+                    total += students.scores[s];
+                }
+                average = total / numOfScores;
+                
+            console.log(`Student ${i} average score: ${average.toFixed(2)} `);
+        }
+}
+
+studentAverage(students);
